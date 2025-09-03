66d9b7503af94d389a501d4e033e44e23bb48d82
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-03 23:11:39 +0300

    feat(app): add __str__ methods to models

--- app/models.py
+++ app/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(auto_now=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
