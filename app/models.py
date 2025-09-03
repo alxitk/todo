fb2c7d0ac80b423f080931fc776e325c23a71bd8
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-03 17:52:05 +0300

    feat(app): add models and admin configuration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Task(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
