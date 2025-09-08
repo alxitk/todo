5dcc1b73e3a64ac5cf95cdf58138898d1422bcb1
Oleksii Drenov <leshadrenev@gmail.com>
2025-09-08 16:22:38 +0300

    feat(tasks): implement task delete functionality

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from app.views import TasksListView, TaskCreateView, TaskUpdateView
+from app.views import (TasksListView,
+                       TaskCreateView,
+                       TaskUpdateView,
+                       TaskDeleteView,)
 
 app_name = "todo"
 
@@ -8,4 +11,5 @@ urlpatterns = [
     path("", TasksListView.as_view(), name="task-list", ),
     path("create/", TaskCreateView.as_view(), name="task-create", ),
     path("update/<int:pk>/", TaskUpdateView.as_view(), name="task-update", ),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete", ),
 ]
